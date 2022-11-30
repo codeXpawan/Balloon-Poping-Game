ea9c3d0210aaf0352740d3db8f3692d44063b4f9
codeXpawan <shahpawan56@gmail.com>
2022-11-30 23:13:33 +0545

    Balloon was shown in the window using pygame

--- Poping.py
+++ Poping.py
@@ -1,7 +1,7 @@
 #import pygame to use pygame
 import pygame
 #import cv2 to use opencv
-import cv2 as cv
+# import cv2 as cv
 pygame.init()    #initialize pygame
 
 #defining the screen width and height
@@ -12,9 +12,9 @@ screen = pygame.display.set_mode((Screen_Width, Screen_Height))
 #setting the title of the window
 pygame.display.set_caption("Poping Game")
 #loading the image of balloon
-blue_balloon = pygame.image.load("Balloon/Blue_Balloon.png")
-red_balloon = pygame.image.load("Balloon/Red_Balloon.png")
-green_balloon = pygame.image.load("Balloon/Green_Balloon.png")
+blue_balloon = pygame.image.load("Resized_Balloon/Blue_Balloon.png")
+red_balloon = pygame.image.load("Resized_Balloon/Red_Balloon.png")
+green_balloon = pygame.image.load("Resized_Balloon/Green_Balloon.png")
 #run until the user asks to quit
 running = True
 while running:
@@ -22,7 +22,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.blit(red_balloon,(0,0))
-    # screen.fill((255, 255, 255))
+    screen.fill((255, 255, 255))
+    screen.blit(blue_balloon,(0,0))
     pygame.display.flip()
 pygame.quit()
