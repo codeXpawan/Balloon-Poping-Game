80bf5bc1ff4bfd6045054cb9a31fd19cc24d666a
codeXpawan <shahpawan56@gmail.com>
2022-12-02 23:26:11 +0545

    detection failed in video

--- Resizing_video.py
+++ Resizing_video.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import matplotlib.pyplot as plt
+import numpy as np
 top = 200
 bottom = 1000
 left = 50
@@ -23,7 +24,31 @@ while video.isOpened():
     
     frame = frame[top:bottom,left:right]
     frame = resizing_video(frame)
-    cv.imshow("Frame",frame)
+    gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    blur = cv.GaussianBlur(gray,(7,7),0)
+    cv.imshow("Blur",blur)
+    canny = cv.Canny(blur,100,150)
+    contours, hierarchies  = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+    cv.imshow("Canny",canny)
+    contours_poly = [None]*len(contours)
+    perimeter = [None]*len(contours)
+    boundRect = [None]*len(contours)
+    for i, c in enumerate(contours):
+        contours_poly[i] = cv.approxPolyDP(c, 3, True)
+        boundRect[i] = cv.minAreaRect(contours_poly[i])
+        perimeter[i] = cv.contourArea(contours_poly[i])
+    for i in range(len(contours)):
+        box = cv.boxPoints(boundRect[i])
+        box = np.int0(box)
+        print(perimeter[i])
+        if perimeter[i]>10:
+            cv.drawContours(frame,[box],0,(0,0,255),2)
+        # cv.rectangle(img, (int(boundRect[i][0]), int(boundRect[i][1])),(int(boundRect[i][0]+boundRect[i][2]), int(boundRect[i][1]+boundRect[i][3])), (0,255,0), 2)
+        else:
+            cv.drawContours(frame, [box], 0, (0,255,0), 2)
+    cv.imshow("Rectangle",frame)
+
+    # cv.imshow("Frame",frame)
     if cv.waitKey(100) == ord('q'):
         break
 video.release()
