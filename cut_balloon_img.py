38492aa2c630ad10fac48e459052ca1d3ed3dca7
codeXpawan <shahpawan56@gmail.com>
2022-12-17 14:22:50 +0545

    find a way to detect the hit

--- cut_balloon_img.py
+++ cut_balloon_img.py
@@ -11,6 +11,7 @@ contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIM
 cnt = []
 area = [None]*len(contours)
 sliced = []
+sliced_gray = []
 for i in range(len(contours)):
     area[i] = cv.contourArea(contours[i])
     # print(area[i])
@@ -21,18 +22,35 @@ for i in range(len(cnt)):
     # print(x,y,w,h)
     # cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
     sliced.append(img[y:y+h, x:x+w])
-    # cv.imshow("Sliced{}".format(i), sliced[i])
-    sliced_blur = cv.GaussianBlur(sliced[i], (9,9), 0)
+    sliced_gray.append(cv.cvtColor(sliced[i],cv.COLOR_BGR2GRAY))
+    sliced_blur = cv.GaussianBlur(sliced_gray[i], (9,9), 0)
     sliced_canny = cv.Canny(sliced_blur, 50, 150)
     sliced_contours,sliced_hierarchy = cv.findContours(sliced_canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
     # cv.drawContours(sliced[i], sliced_contours, -1, (0,255,0), 3)
-    for j in range(len(sliced_contours)):
-        print("for sliced {}".format(i))
-        if cv.contourArea(sliced_contours[j])>500:
-            x,y,w,h = cv.boundingRect(sliced_contours[j])
-            cv.rectangle(sliced[i],(x,y),(x+w,y+h),(0,0,255),2)
-        print(cv.contourArea(sliced_contours[j]))
-        cv.imshow("Sliced{}".format(i), sliced[i])
+    cv.imshow("Sliced{}".format(i), sliced[i])
+# print(len(sliced_contours),i)
+for j in range(len(sliced)):
+    circles = cv.HoughCircles(sliced_gray[j],cv.HOUGH_GRADIENT,1.2,10)
+    print(circles.size)
+    if circles.size > 4:
+        print("hit", j)
+    if circles is not None:
+        circles = np.round(circles[0, :]).astype("int")
+        for (x, y, r) in circles:
+            # draw the circle in the output image, then draw a rectangle
+            # corresponding to the center of the circle
+            print("#")
+            cv.circle(sliced[j], (x, y), r, (0, 255, 0), 4)
+            cv.rectangle(sliced[j], (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+    # print("for sliced {}".format(i))
+    # print("*")
+    # cv.HoughCircles(sliced[j])
+    # if len(sliced_contours[j])>=4:
+    #     print("hit",i)
+    #     # x,y,w,h = cv.boundingRect(sliced_contours[j])
+    #     # cv.rectangle(sliced[i],(x,y),(x+w,y+h),(0,0,255),2)
+    # # print(cv.contourArea(sliced_contours[j]))
+    # cv.imshow("Sliced{}".format(j), sliced[j])
 cv.imshow("Original", img)
 cv.waitKey(0)
 cv.destroyAllWindows()
